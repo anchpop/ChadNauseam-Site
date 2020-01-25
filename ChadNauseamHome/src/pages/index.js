7e63f4816cabbf0a2d40904a345e6120e381e48a
Andre Popovitch <andre@popovit.ch>
2020-01-24 22:20:10 -0500

    added actual info to homepage

--- ChadNauseamHome/src/pages/index.js
+++ ChadNauseamHome/src/pages/index.js
@@ -9,13 +9,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>I'm <b>Chad Nauseam</b>.</p>
+    <p>I make <a href="/youtube">YouTube videos</a>. They're mostly about politics so far but I don't know if they're going to stay that way forever.</p>
+    <p>I also like programming, but only in hipster languages like Rust and Haskell.</p>
+    <p>This website is a work in progress (like most things in my life). The only reason it exists is because I wanted to tell people to follow me on youtube but I was worried they wouldn't know how to spell nauseam. For that reason you can also find this site at <a href="https://chadnauseum.com/">chadnauseum.com</a> as well.</p>
+    <p>If you'd like to talk to me, message me on <a href="/twitter">Twitter</a>, shoot me an email at `contact at chadnauseum dot com`, or join <a href="/discord">my Discord</a>.</p>
   </Layout>
 )
 
