a51126376048a4b37551ec516900ddd246422e0b
Andre Popovitch <andre@popovit.ch>
2020-06-30 17:16:20 -0400

    okay, I'm done playing with colors

--- ChadNauseamHome/src/pages/index.js
+++ ChadNauseamHome/src/pages/index.js
@@ -6,7 +6,7 @@ import Image from "../components/image"
 
 const IndexPage = () => (
   <Layout subtitle="General Info">
-    <p>Check out my <a href="/reasoning-quiz">Policial Reasoning Style Quiz</a>!</p>
+    <p>Check out my <Link href="/reasoning-quiz">Policial Reasoning Style Quiz</Link>!</p>
     <p>I make <a href="/youtube">YouTube videos</a>. They're mostly about politics so far but I don't know if they're going to stay that way forever.</p>
     <p>I also like programming, but only in hipster languages like Rust and Haskell.</p>
     <p>This website is a work in progress (like most things in my life). The only reason it exists is because I wanted to tell people to follow me on youtube but I was worried they wouldn't know how to spell nauseam. For that reason you can also find this site at <a href="https://chadnauseum.com/">chadnauseum.com</a> as well.</p>
