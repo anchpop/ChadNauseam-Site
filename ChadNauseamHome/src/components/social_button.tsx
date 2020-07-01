68f910dd446460e23b2f5643f3c749cf656720b9
Andre Popovitch <andre@popovit.ch>
2020-07-01 16:06:48 -0400

    re-show social buttons on small screens, but just hide the text

--- ChadNauseamHome/src/components/social_button.tsx
+++ ChadNauseamHome/src/components/social_button.tsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const SocialButton = ({ text, color, icon, href }) => {
   return (
-    <div style={{ backgroundColor: color }} className="shadowed socialButton" onClick={() => { window.location = href }}>
-      <img src={icon} className="svg-icon" style={{ marginRight: "4px" }} />
-      <span style={{ fontFamily: "Comic Neue", color: "white" }}>
+    <div style={{ backgroundColor: color }} className="shadowed social-button" onClick={() => { window.location = href }}>
+      <img src={icon} className="svg-icon" />
+      <span style={{ fontFamily: "Comic Neue", color: "white", marginLeft: 4 }} className="social-button-text">
         {text}
       </span>
     </div>
