28bdbf93feb44e0db999d227a376ef849bfd579b
Andre Popovitch <andre@popovit.ch>
2021-02-09 23:21:08 -0500

    fixed typos and errors and stuff

--- ChadNauseamHome/src/pages/music/theory/acoustic-consonance.tsx
+++ ChadNauseamHome/src/pages/music/theory/acoustic-consonance.tsx
@@ -1,36 +1,36 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
 
-import { select } from "d3"
+import { select } from "d3";
 
-import "katex/dist/katex.min.css"
-import { InlineMath, BlockMath } from "react-katex"
+import "katex/dist/katex.min.css";
+import { InlineMath, BlockMath } from "react-katex";
 
-import Layout from "../../../components/layout"
-import Image from "../../../components/image"
-import Tip from "../../../components/tip"
-import Sn from "../../../components/sn"
-import Note from "../../../components/note"
+import Layout from "../../../components/layout";
+import Image from "../../../components/image";
+import Tip from "../../../components/tip";
+import Sn from "../../../components/sn";
+import Note from "../../../components/note";
 
-import useResizeObserver from "../../../utils/resizeObserver"
+import useResizeObserver from "../../../utils/resizeObserver";
 
-import Tippy from "@tippyjs/react"
-import "tippy.js/dist/tippy.css" // optional
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css"; // optional
 
-import ThemeContext from "../../../utils/themeContext"
+import ThemeContext from "../../../utils/themeContext";
 
-const currentTime = () => new Date().getTime() / 1000
+const currentTime = () => new Date().getTime() / 1000;
 
 const Content = ({ time }) => {
-  const { smallScreen } = React.useContext(ThemeContext)
+  const { smallScreen } = React.useContext(ThemeContext);
 
-  const [data, setData] = React.useState([25, 30, 45, 60, 20])
+  const [data, setData] = React.useState([25, 30, 45, 60, 20]);
   //const wrapperRef = React.useRef()
-  const svgRef = React.useRef()
+  const svgRef = React.useRef();
   //const dimensions = useResizeObserver(wrapperRef)
 
   React.useEffect(() => {
-    const svg = select(svgRef.current)
+    const svg = select(svgRef.current);
     svg
       .selectAll("circle")
       .data(data)
@@ -41,8 +41,8 @@ const Content = ({ time }) => {
       )
       .attr("cx", (d, i) => i * 16 + time * 10)
       .attr("cy", (d, i) => 10)
-      .attr("r", (d, i) => d / 10)
-  }, [data, time])
+      .attr("r", (d, i) => d / 10);
+  }, [data, time]);
   return (
     <>
       <p>Test</p>
@@ -52,7 +52,7 @@ const Content = ({ time }) => {
       <h3>
         <InlineMath math="\int_0^\infty x^2 dx" />
       </h3>
-    
+
       <Sn>
         sidenotes test sidenotes test sidenotes test sidenotes test sidenotes
         test sidenotes test sidenotes test sidenotes
@@ -83,22 +83,22 @@ const Content = ({ time }) => {
       <p>{time}</p>
       <p>{smallScreen ? "small" : "big"}</p>
     </>
-  )
-}
+  );
+};
 
 const AcousticConsonance = () => {
-  const [startingTime, setStartingTime] = React.useState(currentTime())
-  const [time, setTime] = React.useState(currentTime())
+  const [startingTime, setStartingTime] = React.useState(currentTime());
+  const [time, setTime] = React.useState(currentTime());
 
   React.useEffect(() => {
-    setStartingTime(currentTime())
-  }, [])
+    setStartingTime(currentTime());
+  }, []);
 
   React.useEffect(() => {
     setInterval(() => {
-      setTime(currentTime())
-    }, 1000 / 60)
-  }, [])
+      setTime(currentTime());
+    }, 1000 / 60);
+  }, []);
   return (
     <Layout
       subtitle="Music Theory - Acoustic Consonance"
@@ -106,7 +106,7 @@ const AcousticConsonance = () => {
     >
       <Content time={time - startingTime} />
     </Layout>
-  )
-}
+  );
+};
 
-export default AcousticConsonance
+export default AcousticConsonance;
