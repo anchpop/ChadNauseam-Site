e2323be06fcba5f4072de6e71568b57c21e77463
Andre Popovitch <andre@popovit.ch>
2021-12-24 18:00:05 +0000

    initial union draft

--- ChadNauseamHome/src/components/image.tsx
+++ ChadNauseamHome/src/components/image.tsx
@@ -3,10 +3,10 @@ import { useStaticQuery, graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image"
 
 
-export const CenteredImg = ({ src, to, style }) => {
+export const CenteredImg = ({ src, to, style, className }) => {
   const Img = (
     <div className="img-container">
-      <img src={src} style={style} />
+      <img src={src} style={style} className={className} />
     </div>
   );
   if (to !== undefined) {
