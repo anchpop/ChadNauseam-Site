de52f5a819a9547c48417bb286d35b3359ee6186
Andre Popovitch <andre@popovit.ch>
2022-02-16 20:03:11 +0000

    delete embed

--- ChadNauseamHome/src/pages/index.tsx
+++ ChadNauseamHome/src/pages/index.tsx
@@ -56,7 +56,6 @@ const IndexPage = () => (
           </li>)
       }
     </ol>
-    <iframe src="https://discord.com/widget?id=563769642699587593&theme=dark" width="350" height="500" allowTransparency={true} frameBorder={0} sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" style={{margin: "auto"}}></iframe>
 
 
   </Layout>
